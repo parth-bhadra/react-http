f0f298dd0e290a22e6143f83672594e8cd827919
parth-bhadra <63583662+parth-bhadra@users.noreply.github.com>
2022-04-19 12:50:05 +0530

    handling http errors

--- src/App.js
+++ src/App.js
@@ -4,33 +4,51 @@ import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
-  const [isLoading, setIsLoading]= useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   const [movies, setMovies] = useState([]);
+  const [error, setError] = useState(null);
   const transformedData = [];
 
   const fetchMoviesHandler = async () => {
     setIsLoading(true);
-    const response = await fetch('https://swapi.dev/api/films/');
-    const data = await response.json();
+    setError(null);
+    try {
+      const response = await fetch('https://swapi.dev/api/films/');
+      // the fetch api does not throw errors in case of status codes, so no use of try catch block
+      // hence we manually throw errors bases of properties of our reponse object
+      // axios has that functionality << use this for your production apps      
+      if (!response.ok) {
+        throw new Error('Something went wrong')
+      }
+      const data = await response.json();
 
-    for (let item of data.results) {
-      transformedData.push({
-        id: item.episode_id,
-        title: item.title,
-        releaseDate: item.release_date,
-        openingText: item.opening_crawl
-      })
+
+
+      for (let item of data.results) {
+        transformedData.push({
+          id: item.episode_id,
+          title: item.title,
+          releaseDate: item.release_date,
+          openingText: item.opening_crawl
+        })
+      }
+      setMovies(transformedData);
+    } catch (error) {
+      setError(error.message);
     }
-    setMovies(transformedData);
     setIsLoading(false);
   }
-  let showContent;
+  let content;
 
-  if(isLoading)
-    showContent = <p>Loading...</p>
+  if (isLoading)
+    content = <p>Loading...</p>
+  else if (error)
+    content = <p>{error}</p>
+  else if (movies.length === 0)
+    content = <p>Found no movies</p>
   else
-    showContent = <MoviesList movies={movies} />
+    content = <MoviesList movies={movies} />
 
 
 
@@ -40,7 +58,7 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        {showContent}
+        {content}
       </section>
     </React.Fragment>
   );
